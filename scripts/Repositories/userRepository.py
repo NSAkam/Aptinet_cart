c7ef8144f5840ec627a55744e06d1895a7f7c132
mahnazfallahh <mahnaz.fallah1213@gmail.com>
2023-10-24 17:07:22 +0330

    feat:add creat user method

--- scripts/Repositories/userRepository.py
+++ scripts/Repositories/userRepository.py
@@ -1,7 +1,9 @@
+import sqlite3
 from PySide2.QtSql import QSqlQuery
 
 from Services.dal import DAL
 from Models.serverUser import ServerUser
+from Models.user import User
 
 
 class UserRepository:
@@ -40,4 +42,22 @@ class UserRepository:
             user.setOffer(query.value(2))
             user.setCode(query.value((3)))
         return user
-            
+    
+    def create_user(self, email):
+        query = QSqlQuery()
+        serveruser_id = 0
+        query.exec_("SELECT id FROM ServerUser as us WHERE email = '"+email+"'")
+        while query.next():
+            serveruser_id = query.value(0)
+        if serveruser_id > 0 :
+            try:
+                if query.exec_("INSERT INTO User(usId) VALUES(serveruser_id)"):
+                    query.exec_("SELECT MAX(id) FROM User")
+                    while query.next():
+                        id = query.value(0)
+                        return id
+            except sqlite3.Error as e:
+                print(f"Error: {e}")
+        return None
+                
+                    
