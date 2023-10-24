8f3b01daf0026c39a054d03cfa17d67bf7c8ec4d
amir jahany <jahany.amir@hotmail.com>
2023-10-24 11:49:12 +0330

    bug fixes

--- scripts/logic.py
+++ scripts/logic.py
@@ -1,10 +1,17 @@
 from PySide2.QtCore import QObject, Signal, Property,Slot,QUrl
 from Services.dateTime import DateTime
-from Services.gpio import *
 from Services.scanner import ScannerWorker 
 import os
 import sys
 
 
 class Logic(QObject): 
-    pass
+    _datetime : DateTime
+
+    def __init__(self)->None:
+        super().__init__()
+        try:
+            self._datetime = DateTime()
+        except:
+            print("\n can not Get dataTime")
+        
