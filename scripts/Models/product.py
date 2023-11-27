3e9b766dbd77ef88647e435485c13adc5fa84e5f
amir jahany <jahany.amir@hotmail.com>
2023-11-27 14:30:22 +0330

    product changed

--- scripts/Models/product.py
+++ scripts/Models/product.py
@@ -25,218 +25,223 @@ class Product(QObject):
     _barcode: str = ""
     _isOffer: bool = False
     _isPlu: bool = False
-    _tax: float = 0.0
+    _tax: bool = False
     _QR: str = ""
+
+    # if (barcode.Length == 13)
+    # {
+    #     return "normal";
+    # }
+    # else if (barcode.Length < 13 && isPlu == true)
+    # {
+    #     return "weighted";
+    # }
+    # else if (barcode.Length < 13 && isPlu == false)
+    # {
+    #     return "counted";
+    # }
+
     _productType: str = ""
-    
-    
-    _dataModelShow:int = 0 # 1 is normal 2 is PLU
-    _productWeightInBasket:int = 0
-    _countInBasket:int = 0
-    
+
+    _dataModelShow: int = 0  # 1 is normal 2 is PLU
+    _productWeightInBasket: int = 0
+    _countInBasket: int = 0
+
     def __init__(self):
         super().__init__()
-        
+
     @Signal
     def changedSignal(self):
         pass
-        
+
     def get_name(self):
         return self._name
-        
+
     def set_name(self, value):
         self._name = value
         self.changedSignal.emit()
-        
+
     name = Property(str, get_name, set_name, notify=changedSignal)
-            
-    
+
     def get_description(self):
         return self._description
-        
+
     def set_description(self, value):
         self._description = value
         self.changedSignal.emit()
-            
-    description = Property(str, get_description, set_description, notify=changedSignal)
-           
+
+    description = Property(str, get_description,
+                           set_description, notify=changedSignal)
+
     def get_rate(self):
         return self._rate
-        
+
     def set_rate(self, value):
         self._rate = value
         self.changedSignal.emit()
-            
+
     rate = Property(int, get_rate, set_rate, notify=changedSignal)
-            
-    
+
     def get_commentCount(self):
         return self._commentCount
-        
+
     def set_commentCount(self, value):
         self._commentCount = value
         self.changedSignal.emit()
-            
-    commentCount = Property(int, get_commentCount, set_commentCount, notify=changedSignal)
-            
+
+    commentCount = Property(int, get_commentCount,
+                            set_commentCount, notify=changedSignal)
+
     def get_w1(self):
         return self._w1
-        
+
     def set_w1(self, value):
         self._w1 = value
         self.changedSignal.emit()
-            
+
     w1 = Property(int, get_w1, set_w1, notify=changedSignal)
-            
+
     def get_w2(self):
         return self._w2
-        
+
     def set_w2(self, value):
         if value:
             self._w2 = value
             self.changedSignal.emit()
-            
+
     w2 = Property(int, get_w2, set_w2, notify=changedSignal)
-            
+
     def get_w3(self):
         return self._w3
-        
+
     def set_w3(self, value):
         self._w3 = value
         self.changedSignal.emit()
-            
+
     w3 = Property(int, get_w3, set_w3, notify=changedSignal)
-            
+
     def get_w4(self):
         return self._w4
-        
+
     def set_w4(self, value):
         self._w4 = value
         self.changedSignal.emit()
-            
+
     w4 = Property(int, get_w4, set_w4, notify=changedSignal)
-            
+
     def get_w5(self):
         return self._w5
-        
+
     def set_w5(self, value):
-            self._w5 = value
-            self.changedSignal.emit()
-            
+        self._w5 = value
+        self.changedSignal.emit()
+
     w5 = Property(int, get_w5, set_w5, notify=changedSignal)
-            
-    
+
     def get_w6(self):
         return self._w6
-        
+
     def set_w6(self, value):
         if value:
             self._w6 = value
             self.changedSignal.emit()
-            
+
     w6 = Property(int, get_w6, set_w6, notify=changedSignal)
-            
-    
+
     def get_w7(self):
         return self._w7
-        
+
     def set_w7(self, value):
         self._w7 = value
         self.changedSignal.emit()
-            
+
     w7 = Property(int, get_w7, set_w7, notify=changedSignal)
-            
-    
+
     def get_w8(self):
         return self._w8
-        
+
     def set_w8(self, value):
         self._w8 = value
         self.changedSignal.emit()
-            
+
     w8 = Property(int, get_w8, set_w8, notify=changedSignal)
-            
-    
+
     def get_w9(self):
         return self._w9
-        
+
     def set_w9(self, value):
         self._w9 = value
         self.changedSignal.emit()
-            
+
     w9 = Property(int, get_w9, set_w9, notify=changedSignal)
-            
-    
+
     def get_w10(self):
         return self._w10
-        
+
     def set_w10(self, value):
         self._w10 = value
         self.changedSignal.emit()
-            
+
     w10 = Property(int, get_w10, set_w10, notify=changedSignal)
-            
-    
+
     def get_price(self):
         return self._price
-        
+
     def set_price(self, value):
         self._price = value
         self.changedSignal.emit()
-            
+
     price = Property(float, get_price, set_price, notify=changedSignal)
-            
-    
+
     def get_finalPrice(self):
         return self._finalPrice
-        
+
     def set_finalPrice(self, value):
         self._finalPrice = value
         self.changedSignal.emit()
-            
-    finalPrice = Property(float, get_finalPrice, set_finalPrice, notify=changedSignal)
-            
-    
+
+    finalPrice = Property(float, get_finalPrice,
+                          set_finalPrice, notify=changedSignal)
+
     def get_meanWeight(self):
         return self._meanWeight
-        
+
     def set_meanWeight(self, value):
         self._meanWeight = value
         self.changedSignal.emit()
-            
-    meanWeight = Property(int, get_meanWeight, set_meanWeight, notify=changedSignal)
-            
-    
+
+    meanWeight = Property(int, get_meanWeight,
+                          set_meanWeight, notify=changedSignal)
+
     def get_tolerance(self):
         return self._tolerance
-        
+
     def set_tolerance(self, value):
         self._tolerance = value
         self.changedSignal.emit()
-            
-    tolerance = Property(int, get_tolerance, set_tolerance, notify=changedSignal)
-            
-    
+
+    tolerance = Property(int, get_tolerance,
+                         set_tolerance, notify=changedSignal)
+
     def get_insertedWeight(self):
         return self._insertedWeight
-        
+
     def set_insertedWeight(self, value):
         self._insertedWeight = value
         self.changedSignal.emit()
-            
-    insertedWeight = Property(int, get_insertedWeight, set_insertedWeight, notify=changedSignal)
-            
-    
+
+    insertedWeight = Property(int, get_insertedWeight,
+                              set_insertedWeight, notify=changedSignal)
+
     def get_barcode(self):
         return self._barcode
-        
+
     def set_barcode(self, value):
         self._barcode = value
         self.changedSignal.emit()
-            
+
     barcode = Property(int, get_barcode, set_barcode, notify=changedSignal)
-    
-    
+
     def get_pic(self):
         if (os.path.isfile("/home/kast/pics/" + self.get_barcode() + ".png") == False):
             return "file:///home/kast/pics/DefaultProduct.png"
@@ -244,75 +249,71 @@ class Product(QObject):
             return "file:///home/kast/pics/" + self.get_barcode + ".png"
 
     pic = Property(str, get_pic, notify=changedSignal)
-            
-            
+
     def get_isOffer(self):
         return self._isOffer
-        
+
     def set_isOffer(self, value):
         self._isOffer = value
         self.changedSignal.emit()
-            
+
     isOffer = Property(int, get_isOffer, set_isOffer, notify=changedSignal)
-            
-    
+
     def get_isPlu(self):
         return self._isPlu
-        
+
     def set_isPlu(self, value):
         self._isPlu = value
         self.changedSignal.emit()
-            
+
     isPlu = Property(int, get_isPlu, set_isPlu, notify=changedSignal)
-            
-    
+
     def get_tax(self):
         return self._tax
-        
+
     def set_tax(self, value):
         self._tax = value
         self.changedSignal.emit()
-            
-    tax = Property(float, get_tax, set_tax, notify=changedSignal)
-    
+
+    tax = Property(bool, get_tax, set_tax, notify=changedSignal)
+
     def get_QR(self):
         return self._QR
-    
+
     def set_QR(self, value):
         self._QR = value
         self.changedSignal.emit()
-            
+
     QR = Property(str, get_QR, set_QR, notify=changedSignal)
 
-    
     def get_productType(self):
         return self._productType
-    
+
     def set_productType(self, value):
         self._productType = value
         self.changedSignal.emit()
-        
-    productType = Property(str, get_productType, set_productType, notify=changedSignal)           
-    
+
+    productType = Property(str, get_productType,
+                           set_productType, notify=changedSignal)
 
     def get_productWeightInBasket(self):
         return self._productWeightInBasket
-    
+
     def set_productWeightInBasket(self, value):
         self._productWeightInBasket = value
         self.changedSignal.emit()
-        
-    productWeightInBasket = Property(int, get_productWeightInBasket, set_productWeightInBasket, notify=changedSignal)           
+
+    productWeightInBasket = Property(
+        int, get_productWeightInBasket, set_productWeightInBasket, notify=changedSignal)
 
     def get_dataModelShow(self):
-        if(self.productType == "weighted"):
+        if (self.productType == "weighted"):
             return 2
         else:
             return 1
 
     dataModelShow = Property(int, get_dataModelShow, notify=changedSignal)
 
-        
     def get_countInBasket(self):
         return self._countInBasket
 
@@ -320,8 +321,8 @@ class Product(QObject):
         self._countInBasket = val
         self.changedSignal.emit()
 
-    countInBasket = Property(int, get_countInBasket, set_countInBasket, notify=changedSignal)
-
+    countInBasket = Property(int, get_countInBasket,
+                             set_countInBasket, notify=changedSignal)
 
     def copy_product(self):
         """
@@ -355,7 +356,6 @@ class Product(QObject):
         product.set_QR(self.QR)
         product.set_productType(self.productType)
         return product
-        
 
     def call_newWeightParameter(self, weight: int):
         """
