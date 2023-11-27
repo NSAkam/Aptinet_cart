b182858faaa157a2e16b5449186f0f7eedbd9c0a
Mizmamad <moradlimohammad@gmail.com>
2023-11-27 19:43:25 +0330

    camera test

--- scripts/Helpers/cameraHelper.py
+++ scripts/Helpers/cameraHelper.py
@@ -3,32 +3,47 @@ from PySide2.QtCore import QObject, Signal, Property, Slot, Qt
 from Services.camera import CameraWorker
 from PySide2.QtGui import QImage
 from PySide2.QtQuick import QQuickImageProvider
+import numpy as np
 
 
 class CameraHelper(QQuickImageProvider):
-
     frameHeight = 184
     FrameWidth = 236
     _camera: CameraWorker
     _image: QImage
 
     def __init__(self):
-        super().__init__(QQuickImageProvider.Image)
         self._camera = CameraWorker()
         self._camera.newFrameReadSignal.connect(self.read_frame)
         self._image = QImage(self.frameHeight, self.FrameWidth, QImage.Format_RGBA8888)
         self._image.fill(Qt.black)
         self.start()
+        super().__init__(QQuickImageProvider.Image)
 
-    newImageReadSignal = Signal()
+    # newImageReadSignal = Signal()
+    @Signal
+    def newImageReadSignal(self):
+        pass
 
     @Slot()
     def read_frame(self):
         frame = self._camera.get_frame()
-        frame = cv2.resize(frame, (self.frameHeight, self.frameHeight))
+        # print("helper. befor resize:")
+        # print(np.shape(frame))
+        try:
+            frame = cv2.resize(frame, (self.frameHeight, self.FrameWidth))
+
+        except:
+            frame = np.ndarray((236, 184))
+
+        # print("helper. after resize:")
+        # print(np.shape(frame))
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        # print("coler convert")
         self._image = QImage(frame, frame.shape[1], frame.shape[0], frame.strides[0], QImage.Format_RGB888)
-        self.newImageReadSignal.emit()
+        # print("Q imqge")
+        # self.newImageReadSignal.emit()
+        # print("image signal")
 
     def requestImage(self, id, p_str, size):
         return self._image
--- scripts/Services/camera.py
+++ scripts/Services/camera.py
@@ -1,3 +1,5 @@
+import time
+
 import cv2
 from PySide2.QtCore import QObject, Signal, Property, Slot, QThread
 import numpy as np
@@ -7,7 +9,7 @@ from datetime import datetime
 class CameraWorker(QThread):
     _cameraID = []
     _camera1: cv2.VideoCapture
-    _camera2: cv2.VideoCapture
+    # _camera2: cv2.VideoCapture
     _frame: np.ndarray
 
     _canReadFrame: bool
@@ -17,13 +19,11 @@ class CameraWorker(QThread):
 
     def __init__(self):
         super().__init__()
-        self.find_cameraID()
-        self._camera1 = cv2.VideoCapture(self._cameraID[0])
-        self._camera2 = cv2.VideoCapture(self._cameraID[1])
-        self._camera1.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-        self._camera1.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-        self._camera2.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-        self._camera2.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+        # self.find_cameraID()
+        # self._camera1 = cv2.VideoCapture(self._cameraID[0])
+        # self._camera2 = cv2.VideoCapture(self._cameraID[1])
+        self._camera1 = cv2.VideoCapture(1)
+        # self._camera2 = cv2.VideoCapture(1)
         self._canReadFrame = False
         # self._frame = np.ndarray((480, 640, 3))
 
@@ -34,15 +34,10 @@ class CameraWorker(QThread):
 
     def find_cameraID(self):
         for i in range(10):
-            try:
-                camera = cv2.VideoCapture(i)
-                if not camera.isOpened():
-                    print("cannot open camera ")
-                ret, frame = camera.read()
-                if frame is not None:
-                    self._cameraID.append(int(i))
-            except:
-                pass
+            camera = cv2.VideoCapture(i)
+            ret, frame = camera.read()
+            if frame is not None:
+                self._cameraID.append(int(i))
 
     def run(self):
         self._canReadFrame = True
@@ -50,18 +45,27 @@ class CameraWorker(QThread):
         while self._canReadFrame:
             if self._readFromCamera1:
                 _, self._frame = self._camera1.read()
-                self.newFrameReadSignal.emit()
-                now = datetime.now()
-                if (now - self._lastSwitchTime).seconds >= self._switchTime:
-                    self._lastSwitchTime = now
-                    self._readFromCamera1 = False
-            else:
-                _, self._frame = self._camera2.read()
-                self.newFrameReadSignal.emit()
-                now = datetime.now()
-                if (now - self._lastSwitchTime).seconds >= self._switchTime:
-                    self._lastSwitchTime = now
-                    self._readFromCamera1 = True
+                if self._camera1.isOpened():
+                # if self._frame is not None:
+                #     print(np.shape(self._frame))
+                #     time.sleep(0.5)
+                    self.newFrameReadSignal.emit()
+                    # now = datetime.now()
+                    # if (now - self._lastSwitchTime).seconds >= self._switchTime:
+                    #     self._lastSwitchTime = now
+                    #     self._readFromCamera1 = False
+                else:
+                    print("cam1 no frame")
+            time.sleep(1)
+            # else:
+            #     _, self._frame = self._camera2.read()
+            #     self.newFrameReadSignal.emit()
+            #     now = datetime.now()
+            #     if (now - self._lastSwitchTime).seconds >= self._switchTime:
+            #         self._lastSwitchTime = now
+            #         self._readFromCamera1 = True
+            # # self._canReadFrame = False
+
 
     def stop(self):
         self._canReadFrame = False
