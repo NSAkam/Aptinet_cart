cce5376dfefbc667e5e31fec91688171e45ff2da
amir jahany <jahany.amir@hotmail.com>
2023-11-27 18:55:12 +0330

    test

--- scripts/Services/camera.py
+++ scripts/Services/camera.py
@@ -20,6 +20,10 @@ class CameraWorker(QThread):
         self.find_cameraID()
         self._camera1 = cv2.VideoCapture(self._cameraID[0])
         self._camera2 = cv2.VideoCapture(self._cameraID[1])
+        self._camera1.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        self._camera1.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+        self._camera2.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        self._camera2.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
         self._canReadFrame = False
         # self._frame = np.ndarray((480, 640, 3))
 
@@ -31,8 +35,6 @@ class CameraWorker(QThread):
     def find_cameraID(self):
         for i in range(10):
             camera = cv2.VideoCapture(i)
-            camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-            camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
             ret, frame = camera.read()
             if frame is not None:
                 self._cameraID.append(int(i))
