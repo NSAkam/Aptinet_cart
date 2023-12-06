aa01ef574e93809278d6a9ae646ae28927398d5c
amir jahany <jahany.amir@hotmail.com>
2023-12-07 02:46:45 +0330

    test

--- scripts/Services/nfc.py
+++ scripts/Services/nfc.py
@@ -1,13 +1,9 @@
-#! /usr/bin/env python3
-"""
-Sample for python PCSC wrapper module: Locate cards in the system
+"""PCSCReader: concrete reader class for PCSC Readers
 
-__author__ = "http://www.gemalto.com"
+__author__ = "gemalto http://www.gemalto.com"
 
 Copyright 2001-2012 gemalto
 Author: Jean-Daniel Aussel, mailto:jean-daniel.aussel@gemalto.com
-Copyright 2010 Ludovic Rousseau
-Author: Ludovic Rousseau, mailto:ludovic.rousseau@free.fr
 
 This file is part of pyscard.
 
@@ -26,105 +22,118 @@ along with pyscard; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 """
 
+from smartcard.CardConnectionDecorator import CardConnectionDecorator
+from smartcard.reader.Reader import Reader
+from smartcard.pcsc.PCSCContext import PCSCContext
+from smartcard.pcsc.PCSCCardConnection import PCSCCardConnection
+from smartcard.Exceptions import *
+from smartcard.pcsc.PCSCExceptions import *
 from smartcard.scard import *
-import sys
 
-if 'winscard' == resourceManager:
 
-    znewcardName = 'dummy-card'
-    znewcardATR = [0x3B, 0x77, 0x94, 0x00, 0x00, 0x82, 0x30, 0x00, 0x13,
-                   0x6C, 0x9F, 0x22]
-    znewcardMask = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-                    0xFF, 0xFF, 0xFF]
+def __PCSCreaders__(hcontext, groups=[]):
+    """Returns the list of PCSC smartcard readers in PCSC group.
 
-    try:
+    If group is not specified, returns the list of all PCSC smartcard readers.
+    """
+
+    # in case we have a string instead of a list
+    if isinstance(groups, type("")):
+        groups = [groups]
+    hresult, readers = SCardListReaders(hcontext, groups)
+    if hresult != 0:
+        if hresult == SCARD_E_NO_READERS_AVAILABLE:
+            readers = []
+        elif hresult == SCARD_E_SERVICE_STOPPED:
+            raise CardServiceStoppedException(hresult=hresult)
+        elif hresult == SCARD_E_NO_SERVICE:
+            raise CardServiceNotFoundException(hresult=hresult)
+        else:
+            raise ListReadersException(hresult)
+
+    return readers
+
+
+class PCSCReader(Reader):
+    """PCSC reader class."""
+
+    def __init__(self, readername):
+        """Constructs a new PCSC reader."""
+        Reader.__init__(self, readername)
+
+    def addtoreadergroup(self, groupname):
+        """Add reader to a reader group."""
+
         hresult, hcontext = SCardEstablishContext(SCARD_SCOPE_USER)
-        if hresult != SCARD_S_SUCCESS:
-            raise scard.error(
-                'Failed to establish context: ' +
-                SCardGetErrorMessage(hresult))
-        print('Context established!')
+        if 0 != hresult:
+            raise EstablishContextException(hresult)
+        try:
+            hresult = SCardIntroduceReader(hcontext, self.name, self.name)
+            if 0 != hresult and SCARD_E_DUPLICATE_READER != hresult:
+                raise IntroduceReaderException(hresult, self.name)
+            hresult = SCardAddReaderToGroup(hcontext, self.name, groupname)
+            if 0 != hresult:
+                raise AddReaderToGroupException(hresult, self.name, groupname)
+        finally:
+            hresult = SCardReleaseContext(hcontext)
+            if 0 != hresult:
+                raise ReleaseContextException(hresult)
+
+    def removefromreadergroup(self, groupname):
+        """Remove a reader from a reader group"""
+
+        hresult, hcontext = SCardEstablishContext(SCARD_SCOPE_USER)
+        if 0 != hresult:
+            raise EstablishContextException(hresult)
+        try:
+            hresult = SCardRemoveReaderFromGroup(hcontext, self.name,
+                groupname)
+            if 0 != hresult:
+                raise RemoveReaderFromGroupException(hresult, self.name,
+                    groupname)
+        finally:
+            hresult = SCardReleaseContext(hcontext)
+            if 0 != hresult:
+                raise ReleaseContextException(hresult)
+
+    def createConnection(self):
+        """Return a card connection thru PCSC reader."""
+        return CardConnectionDecorator(PCSCCardConnection(self.name))
+
+    class Factory:
+
+        def create(readername):
+            return PCSCReader(readername)
+        create = staticmethod(create)
+
+    def readers(groups=[]):
+        creaders = []
+        hcontext = PCSCContext().getContext()
 
         try:
-            hresult, readers = SCardListReaders(hcontext, [])
-            if hresult != SCARD_S_SUCCESS:
-                raise scard.error(
-                    'Failed to list readers: ' +
-                    SCardGetErrorMessage(hresult))
-            print('PCSC Readers:', readers)
+            pcsc_readers = __PCSCreaders__(hcontext, groups)
+        except (CardServiceStoppedException, CardServiceNotFoundException):
+            hcontext = PCSCContext.renewContext()
+            pcsc_readers = __PCSCreaders__(hcontext, groups)
 
-            # introduce a card (forget first in case it is already present)
-            hresult = SCardForgetCardType(hcontext, znewcardName)
-            print('Introducing card ' + znewcardName)
-            hresult = SCardIntroduceCardType(hcontext, znewcardName, [],
-                                             [], znewcardATR, znewcardMask)
-            if hresult != SCARD_S_SUCCESS:
-                if hresult == ERROR_ALREADY_EXISTS:
-                    print('Card already exists')
-                else:
-                    raise error(
-                        'Failed to introduce card type: ' +
-                        SCardGetErrorMessage(hresult))
+        for reader in pcsc_readers:
+            creaders.append(PCSCReader.Factory.create(reader))
+        return creaders
+    readers = staticmethod(readers)
 
-            hresult, cards = SCardListCards(hcontext, [], [])
-            if hresult != SCARD_S_SUCCESS:
-                raise error('Failure to list cards')
-            print('Cards:', cards)
+if __name__ == '__main__':
+    from smartcard.util import *
+    SELECT = [0xA0, 0xA4, 0x00, 0x00, 0x02]
+    DF_TELECOM = [0x7F, 0x10]
 
-            readerstates = []
-            for i in range(len(readers)):
-                readerstates += [(readers[i], SCARD_STATE_UNAWARE)]
-            print(readerstates)
-
-            hresult, newstates = SCardLocateCards(
-                hcontext,
-                cards,
-                readerstates)
-            for i in newstates:
-                reader, eventstate, atr = i
-                print(reader, end=' ')
-                for b in atr:
-                    print("0x%.2X" % b, end=' ')
-                print("")
-                if eventstate & SCARD_STATE_ATRMATCH:
-                    print('Card found')
-                if eventstate & SCARD_STATE_UNAWARE:
-                    print('State unware')
-                if eventstate & SCARD_STATE_IGNORE:
-                    print('Ignore reader')
-                if eventstate & SCARD_STATE_UNAVAILABLE:
-                    print('Reader unavailable')
-                if eventstate & SCARD_STATE_EMPTY:
-                    print('Reader empty')
-                if eventstate & SCARD_STATE_PRESENT:
-                    print('Card present in reader')
-                if eventstate & SCARD_STATE_EXCLUSIVE:
-                    print('Card allocated for exclusive use')
-                if eventstate & SCARD_STATE_INUSE:
-                    print('Card in use but can be shared')
-                if eventstate & SCARD_STATE_MUTE:
-                    print('Card is mute')
-                if eventstate & SCARD_STATE_CHANGED:
-                    print('State changed')
-                if eventstate & SCARD_STATE_UNKNOWN:
-                    print('State unknowned')
-
-        finally:
-            hresult = SCardForgetCardType(hcontext, znewcardName)
-            hresult = SCardReleaseContext(hcontext)
-            if hresult != SCARD_S_SUCCESS:
-                raise error(
-                    'Failed to release context: ' +
-                    SCardGetErrorMessage(hresult))
-            print('Released context.')
-
-    except error as e:
-        print(e)
-
-elif 'pcsclite' == resourceManager:
-    print('SCardLocateCards not supported by pcsc lite')
-
-
-if 'win32' == sys.platform:
-    print('press Enter to continue')
-    sys.stdin.read(1)
+    creaders = PCSCReader.readers()
+    for reader in creaders:
+        try:
+            print(reader.name)
+            connection = reader.createConnection()
+            connection.connect()
+            print(toHexString(connection.getATR()))
+            data, sw1, sw2 = connection.transmit(SELECT + DF_TELECOM)
+            print("{:02X} {:02X}".format(sw1, sw2))
+        except NoCardException:
+            print('no card in reader')
