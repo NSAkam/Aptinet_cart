b9510132413c465d539fa61c517036bf50bcd901
amir jahany <jahany.amir@hotmail.com>
2023-12-07 02:41:48 +0330

    test

--- scripts/Services/nfc.py
+++ scripts/Services/nfc.py
@@ -1,10 +1,8 @@
-"""Smart card reader monitoring classes.
+#! /usr/bin/env python3
+"""Unit tests for SCardBeginTransaction/SCardEndTransaction.
 
-ReaderObserver is a base class for objects that are to be notified
-upon smartcard reader insertion/removal.
-
-ReaderMonitor is a singleton object notifying registered ReaderObservers
-upon reader insertion/removal.
+This test case can be executed individually, or with all other test cases
+thru testsuite_scard.py.
 
 __author__ = "http://www.gemalto.com"
 
@@ -28,214 +26,78 @@ along with pyscard; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 """
 
-from threading import Thread, Event
-from time import sleep
-import traceback
 
-import smartcard.System
-from smartcard.Observer import Observer
-from smartcard.Observer import Observable
-from smartcard.Synchronization import *
+import unittest
+from smartcard.scard import *
 
+# import local_config for reader/card configuration
+# configcheck.py is generating local_config.py in
+# the test suite.
+import sys
+sys.path += ['..']
 
-# ReaderObserver interface
-class ReaderObserver(Observer):
-    """
-    ReaderObserver is a base abstract class for objects that are to be notified
-    upon smartcard reader insertion/removal.
-    """
-
-    def __init__(self):
-        pass
-
-    def update(self, observable, handlers):
-        """Called upon reader insertion/removal.
-
-        @param observable:
-        @param handlers:
-          - addedreaders: list of added readers causing notification
-          - removedreaders: list of removed readers causing notification
-        """
-        pass
+try:
+    from local_config import expectedATRs, expectedReaders
+except ImportError:
+    print('execute test suite first to generate the local_config.py file')
+    sys.exit()
 
 
-class ReaderMonitor(Observable):
-    """Class that monitors reader insertion/removal.
-    and notify observers
+class testcase_transaction(unittest.TestCase):
+    """Test scard API for SCardBegin/EndTransaction"""
 
-    note: a reader monitoring thread will be running
-    as long as the reader monitor has observers, or ReaderMonitor.stop()
-    is called.
+    def setUp(self):
+        hresult, self.hcontext = SCardEstablishContext(SCARD_SCOPE_USER)
+        self.assertEqual(hresult, 0)
+        hresult, self.readers = SCardListReaders(self.hcontext, [])
+        self.assertEqual(hresult, 0)
 
-    It implements the shared state design pattern, where objects
-    of the same type all share the same state, in our case essentially
-    the ReaderMonitoring Thread. Thanks to Frank Aune for implementing
-    the shared state pattern logics.
-    """
+    def tearDown(self):
+        hresult = SCardReleaseContext(self.hcontext)
+        self.assertEqual(hresult, 0)
 
-    __shared_state = {}
+    def _transaction(self, r):
+        if r < len(expectedATRs) and [] != expectedATRs[r]:
+            hresult, hcard, dwActiveProtocol = SCardConnect(
+                self.hcontext,
+                self.readers[r],
+                SCARD_SHARE_SHARED,
+                SCARD_PROTOCOL_T0 | SCARD_PROTOCOL_T1)
+            self.assertEqual(hresult, 0)
 
-    def __init__(self, startOnDemand=True, readerProc=smartcard.System.readers,
-                 period=1):
-        self.__dict__ = self.__shared_state
-        Observable.__init__(self)
-        self.startOnDemand = startOnDemand
-        self.readerProc = readerProc
-        self.period = period
-        if self.startOnDemand:
-            self.rmthread = None
-        else:
-            self.rmthread = ReaderMonitoringThread(self, self.readerProc,
-                                                   self.period)
-            self.rmthread.start()
-
-    def addObserver(self, observer):
-        """Add an observer."""
-        Observable.addObserver(self, observer)
-
-        # If self.startOnDemand is True, the reader monitoring
-        # thread only runs when there are observers.
-        if self.startOnDemand:
-            if 0 < self.countObservers():
-                if not self.rmthread:
-                    self.rmthread = ReaderMonitoringThread(
-                        self,
-                        self.readerProc, self.period)
-
-                    # start reader monitoring thread in another thread to
-                    # avoid a deadlock; addObserver and notifyObservers called
-                    # in the ReaderMonitoringThread run() method are
-                    # synchronized
-                    try:
-                        # Python 3.x
-                        import _thread
-                        _thread.start_new_thread(self.rmthread.start, ())
-                    except:
-                        # Python 2.x
-                        import thread
-                        thread.start_new_thread(self.rmthread.start, ())
-        else:
-            observer.update(self, (self.rmthread.readers, []))
-
-    def deleteObserver(self, observer):
-        """Remove an observer."""
-        Observable.deleteObserver(self, observer)
-        # If self.startOnDemand is True, the reader monitoring
-        # thread is stopped when there are no more observers.
-        if self.startOnDemand:
-            if 0 == self.countObservers():
-                self.rmthread.stop()
-                del self.rmthread
-                self.rmthread = None
-
-    def __str__(self):
-        return self.__class__.__name__
-
-
-synchronize(ReaderMonitor,
-            "addObserver deleteObserver deleteObservers " +
-            "setChanged clearChanged hasChanged " +
-            "countObservers")
-
-
-class ReaderMonitoringThread(Thread):
-    """Reader insertion thread.
-    This thread polls for pcsc reader insertion, since no
-    reader insertion event is available in pcsc.
-    """
-
-    __shared_state = {}
-
-    def __init__(self, observable, readerProc, period):
-        self.__dict__ = self.__shared_state
-        Thread.__init__(self)
-        self.observable = observable
-        self.stopEvent = Event()
-        self.stopEvent.clear()
-        self.readers = []
-        self.setDaemon(True)
-        self.setName('smartcard.ReaderMonitoringThread')
-        self.readerProc = readerProc
-        self.period = period
-
-    def run(self):
-        """Runs until stopEvent is notified, and notify
-        observers of all reader insertion/removal.
-        """
-        while not self.stopEvent.isSet():
             try:
-                # no need to monitor if no observers
-                if 0 < self.observable.countObservers():
-                    currentReaders = self.readerProc()
-                    addedReaders = []
-                    removedReaders = []
+                hresult = SCardBeginTransaction(hcard)
+                self.assertEqual(hresult, 0)
 
-                    if currentReaders != self.readers:
-                        for reader in currentReaders:
-                            if reader not in self.readers:
-                                addedReaders.append(reader)
-                        for reader in self.readers:
-                            if reader not in currentReaders:
-                                removedReaders.append(reader)
+                hresult, reader, state, protocol, atr = SCardStatus(hcard)
+                self.assertEqual(hresult, 0)
+                self.assertEqual(reader, expectedReaders[r])
+                self.assertEqual(atr, expectedATRs[r])
 
-                        if addedReaders or removedReaders:
-                            # Notify observers
-                            self.readers = []
-                            for r in currentReaders:
-                                self.readers.append(r)
-                            self.observable.setChanged()
-                            self.observable.notifyObservers((addedReaders,
-                                                            removedReaders))
+                hresult = SCardEndTransaction(hcard, SCARD_LEAVE_CARD)
+                self.assertEqual(hresult, 0)
 
-                # wait every second on stopEvent
-                self.stopEvent.wait(self.period)
+            finally:
+                hresult = SCardDisconnect(hcard, SCARD_UNPOWER_CARD)
+                self.assertEqual(hresult, 0)
 
-            except Exception:
-                # FIXME Tighten the exceptions caught by this block
-                traceback.print_exc()
-                # Most likely raised during interpreter shutdown due
-                # to unclean exit which failed to remove all observers.
-                # To solve this, we set the stop event and pass the
-                # exception to let the thread finish gracefully.
-                self.stopEvent.set()
+    def test_transaction_reader0(self):
+        testcase_transaction._transaction(self, 0)
 
-    def stop(self):
-        self.stopEvent.set()
-        self.join()
+    def test_transaction_reader1(self):
+        testcase_transaction._transaction(self, 1)
+
+    def test_transaction_reader2(self):
+        testcase_transaction._transaction(self, 2)
+
+    def test_transaction_reader3(self):
+        testcase_transaction._transaction(self, 3)
 
 
-if __name__ == "__main__":
-    print('insert or remove readers in the next 20 seconds')
+def suite():
+    suite1 = unittest.makeSuite(testcase_transaction)
+    return unittest.TestSuite(suite1)
 
-    # a simple reader observer that prints added/removed readers
-    class printobserver(ReaderObserver):
 
-        def __init__(self, obsindex):
-            self.obsindex = obsindex
-
-        def update(self, observable, handlers):
-            addedreaders, removedreaders = handlers
-            print("%d - added:   " % self.obsindex, addedreaders)
-            print("%d - removed: " % self.obsindex, removedreaders)
-
-    class testthread(Thread):
-
-        def __init__(self, obsindex):
-            Thread.__init__(self)
-            self.readermonitor = ReaderMonitor()
-            self.obsindex = obsindex
-            self.observer = None
-
-        def run(self):
-            # create and register observer
-            self.observer = printobserver(self.obsindex)
-            self.readermonitor.addObserver(self.observer)
-            sleep(20)
-            self.readermonitor.deleteObserver(self.observer)
-
-    t1 = testthread(1)
-    t2 = testthread(2)
-    t1.start()
-    t2.start()
-    t1.join()
-    t2.join()
+if __name__ == '__main__':
+    unittest.main()
