c3a7950f60d163c0a4c0dbef871ccf60140f0b13
amir jahany <jahany.amir@hotmail.com>
2023-12-08 23:36:15 +0330

    test

--- scripts/Repositories/configRepository.py
+++ scripts/Repositories/configRepository.py
@@ -38,8 +38,8 @@ class ConfigRepositories():
     
     def get_lang(self):
         query = QSqlQuery()
-        lang = -1
-        query.exec_("select Config from lang LIMIT 1")
+        lang = -1  
+        query.exec_("select lang from Config LIMIT 1")
         while query.next():
             lang = query.value(0)
         return lang
