68a982f074029e071764eb60062d5fb1c76d7e96
Mizmamad <moradlimohammad@gmail.com>
2023-12-08 15:18:27 +0330

    test

--- scripts/main.py
+++ scripts/main.py
@@ -3,6 +3,7 @@ from PySide2.QtCore import Qt
 from PySide2.QtGui import QGuiApplication,QFontDatabase,QFont
 from PySide2.QtQml import QQmlApplicationEngine,qmlRegisterType,QQmlDebuggingEnabler
 
+# from Services.battery import Battery
 from logic import Logic
 from Services.camera import CameraWorker
 from Helpers.cameraHelper import CameraHelper
@@ -17,7 +18,9 @@ if __name__ == "__main__":
 
     id = QFontDatabase.addApplicationFont("../Assets/Archivo/Archivo-Regular.ttf")
     app.setFont(QFont(QFontDatabase.applicationFontFamilies(id)[0]))
-    
+
+    # qmlRegisterType(Battery, "KAST.Battery", 1, 0, "Battery")
+
     qmlRegisterType(Logic, "KAST.Logic" , 1, 0 ,"Logic")
     
     engine = QQmlApplicationEngine()
