9f2a8b682b11ad0e74ff22349f01c6e6e17a44e1
amir jahany <jahany.amir@hotmail.com>
2023-12-10 01:28:16 +0330

    test

---
+++ scripts/Models/Helpers/langModel.py
@@ -0,0 +1,60 @@
+from ast import Dict
+from typing import Any
+from PySide2.QtCore import Signal, Property, QAbstractListModel, QModelIndex, Slot
+from PySide2.QtCore import QModelIndex, Qt, QByteArray
+from Models.lang import Lang
+
+
+class LangModel(QAbstractListModel):
+    NameRole = Qt.UserRole
+
+    m_data = [Lang]
+
+    def __init__(self):
+        super().__init__()
+        self.m_data = []
+
+    @Signal
+    def changed(self):
+        pass
+
+    # Function #################################################################
+
+    def rowCount(self, parent) -> int:  # Override
+        if (parent.isValid()):
+            return 0
+        return len(self.m_data)
+
+    def data(self, index, role: int) -> Any:  # Override
+        if (not index.isValid()):
+            return
+        prod = self.m_data[index.row()]
+        if (role == self.NameRole):
+            return prod.name
+        else:
+            return None
+
+    def roleNames(self) -> Dict:  # Override
+        default = super().roleNames()
+        default[self.NameRole] = QByteArray(b"name")
+        return default
+
+    def clearData(self):
+        self.beginResetModel()
+        self.m_data.clear()
+        self.changed.emit()
+        self.endResetModel()
+
+    def count(self) -> int:
+        return len(self.m_data)
+
+    def insert_languageList(self, langs: [Lang]):
+        self.beginResetModel()
+        for l in langs:
+            self.m_data.append(l)
+        self.endResetModel()
+
+    def initialize_productList(self, prods: [Lang]):
+        self.beginResetModel()
+        self.m_data = prods
+        self.endResetModel()
---
+++ scripts/Models/lang.py
@@ -0,0 +1,22 @@
+from PySide2.QtCore import QObject, Signal, Property
+
+
+class Lang(QObject):
+    _name: str = ""
+
+    @Signal
+    def changedSignal(self):
+        pass
+
+    def __init__(self, name):
+        self.set_name(name)
+        super().__init__()
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, value):
+        self._name = value
+        self.changedSignal.emit()
+
+    name = Property(str, get_name, set_name, notify=changedSignal)
