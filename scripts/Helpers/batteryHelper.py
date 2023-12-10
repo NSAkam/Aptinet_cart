92e2a951051a60405bfa77e83bb436e4d003524f
Mizmamad <moradlimohammad@gmail.com>
2023-12-10 18:56:56 +0330

    battery level test

--- scripts/Helpers/batteryHelper.py
+++ scripts/Helpers/batteryHelper.py
@@ -4,24 +4,20 @@ from statistics import mean
 
 
 class BatteryHelper(QObject):
-    _batteryWorker: BatteryWorker
+    _batteryWorker = BatteryWorker
     _batteryLevel: int
-    _noiseReductionBuffer = []
-    _inStartUp: int
-
-    ### Setting ###################
-    _noiseReductionBufferSize: int = 15
 
     def __init__(self):
         super().__init__()
         self._batteryWorker = BatteryWorker()
         self._batteryWorker.newLevelSignal.connect(self.new_levelRead)
+        self._batteryWorker.chargingSignal.connect(self.charging)
+        self._batteryWorker.stopChargingSignal.connect(self.stop_charging)
         self.set_batteryLevel(50)
-        self._inStartUp = 0
 
     batteryLevelChangedSignal = Signal()
-    chargingSignal = Signal()
     stopChargingSignal = Signal()
+    chargingSignal = Signal()
 
     def get_batteryLevel(self):
         return self._batteryLevel
@@ -32,39 +28,22 @@ class BatteryHelper(QObject):
 
     batteryLevel = Property(int, get_batteryLevel, set_batteryLevel, notify=batteryLevelChangedSignal)
 
-    @Slot()
-    def new_levelRead(self):
-        if self._inStartUp < self._noiseReductionBufferSize:
-            self._noiseReductionBuffer.append(self._batteryWorker.get_level())
-            self._inStartUp += 1
-
-        elif self._inStartUp == self._noiseReductionBufferSize:
-            self._noiseReductionBuffer.pop(0)
-            self._noiseReductionBuffer.append(self._batteryWorker.get_level())
-            self.set_batteryLevel(int(mean(self._noiseReductionBuffer)))
-            self._inStartUp += 1
-
+    @Slot(int)
+    def new_levelRead(self, level: int):
+        if level < 0:
+            self.set_batteryLevel(0)
+        elif level > 100:
+            self.set_batteryLevel(100)
         else:
-            self._noiseReductionBuffer.pop(0)
-            self._noiseReductionBuffer.append(self._batteryWorker.get_level())
+            self.set_batteryLevel(level)
 
-            newLevel = self._batteryWorker.outlierRemover(self._noiseReductionBuffer, outlier_margin=1)
-            if newLevel < self._batteryLevel:
-                canChange = True
-                for level in self._noiseReductionBuffer:
-                    if self._batteryLevel < level:
-                        canChange = False
-                if canChange:
-                    self.stopChargingSignal.emit()
-                    self.set_batteryLevel(self._batteryLevel - 1)
+    @Slot()
+    def charging(self):
+        self.chargingSignal.emit()
 
-            elif newLevel > self._batteryLevel:
-                canChange = True
-                for level in self._noiseReductionBuffer:
-                    if level < self._batteryLevel:
-                        canChange = False
-                if canChange:
-                    self.chargingSignal.emit()
+    @Slot()
+    def stop_charging(self):
+        self.stopChargingSignal.emit()
 
     def start(self):
         self._batteryWorker.start()
@@ -72,3 +51,72 @@ class BatteryHelper(QObject):
     def stop(self):
         self._batteryWorker.stop()
 
+# class BatteryHelper(QObject):
+#     _batteryWorker: BatteryWorker
+#     _batteryLevel: int
+#     _noiseReductionBuffer = []
+#     _inStartUp: int
+#
+#     ### Setting ###################
+#     _noiseReductionBufferSize: int = 15
+#
+#     def __init__(self):
+#         super().__init__()
+#         self._batteryWorker = BatteryWorker()
+#         self._batteryWorker.newLevelSignal.connect(self.new_levelRead)
+#         self.set_batteryLevel(50)
+#         self._inStartUp = 0
+#
+#     batteryLevelChangedSignal = Signal()
+#     chargingSignal = Signal()
+#     stopChargingSignal = Signal()
+#
+#     def get_batteryLevel(self):
+#         return self._batteryLevel
+#
+#     def set_batteryLevel(self, v: int):
+#         self._batteryLevel = v
+#         self.batteryLevelChangedSignal.emit()
+#
+#     batteryLevel = Property(int, get_batteryLevel, set_batteryLevel, notify=batteryLevelChangedSignal)
+#
+#     @Slot()
+#     def new_levelRead(self):
+#         if self._inStartUp < self._noiseReductionBufferSize:
+#             self._noiseReductionBuffer.append(self._batteryWorker.get_level())
+#             self._inStartUp += 1
+#
+#         elif self._inStartUp == self._noiseReductionBufferSize:
+#             self._noiseReductionBuffer.pop(0)
+#             self._noiseReductionBuffer.append(self._batteryWorker.get_level())
+#             self.set_batteryLevel(int(mean(self._noiseReductionBuffer)))
+#             self._inStartUp += 1
+#
+#         else:
+#             self._noiseReductionBuffer.pop(0)
+#             self._noiseReductionBuffer.append(self._batteryWorker.get_level())
+#
+#             newLevel = self._batteryWorker.outlierRemover(self._noiseReductionBuffer, outlier_margin=1)
+#             if newLevel < self._batteryLevel:
+#                 canChange = True
+#                 for level in self._noiseReductionBuffer:
+#                     if self._batteryLevel < level:
+#                         canChange = False
+#                 if canChange:
+#                     # self.stopChargingSignal.emit()
+#                     self.set_batteryLevel(self._batteryLevel - 1)
+#
+#             elif newLevel > self._batteryLevel:
+#                 canChange = True
+#                 for level in self._noiseReductionBuffer:
+#                     if level < self._batteryLevel:
+#                         canChange = False
+#                 if canChange:
+#                     self.chargingSignal.emit()
+#
+#     def start(self):
+#         self._batteryWorker.start()
+#
+#     def stop(self):
+#         self._batteryWorker.stop()
+#
--- scripts/Services/battery.py
+++ scripts/Services/battery.py
@@ -8,13 +8,18 @@ from PySide2.QtCore import QObject, Signal, Property, Slot, QUrl, QThread
 
 class BatteryWorker(QThread):
     _level: int = 50
+    _batteryLevel: int = 50
     _elecNoiseReducBufferSize: int
+    _noiseReductionBufferSize: int
     _elecNoiseReducBuffer = []
+    _noiseReductionBuffer = []
     _minElecNumber: int
     _maxElecNumber: int
     _i2cBus: smbus
     _devAddress: int
     _canReadBarcode: bool
+    _initialElecNoiseReduceBuffer: int = 0
+    _initialNoiseReduceBuffer: int = 0
 
     def __init__(self):
         super().__init__()
@@ -22,6 +27,7 @@ class BatteryWorker(QThread):
         self._devAddress = 0x4d
 
         self._elecNoiseReducBufferSize = 15
+        self._noiseReductionBufferSize = 15
 
         self._minElecNumber = 4096  # MCP3221 maximum electrical number
         self._maxElecNumber = 0  # MCP3221 minimum electrical number
@@ -37,7 +43,10 @@ class BatteryWorker(QThread):
 
         self._canReadBarcode = True
 
-    newLevelSignal = Signal()
+    newLevelSignal = Signal(int)
+    stopChargingSignal = Signal()
+    chargingSignal = Signal()
+
 
     def get_level(self):
         return self._level
@@ -49,17 +58,27 @@ class BatteryWorker(QThread):
             self._level = 100
         else:
             self._level = v
-        self.newLevelSignal.emit()
+
+    def get_batteryLevel(self):
+        return self._batteryLevel
+
+    def set_batteryLevel(self, v: int):
+        if v < 0:
+            self._batteryLevel = 0
+        elif v > 100:
+            self._batteryLevel = 100
+        else:
+            self._batteryLevel = v
 
     def run(self):
         try:
-            inStartUp: int = 0
+            self._initialElecNoiseReduceBuffer: int = 0
             while self._canReadBarcode:
-                if inStartUp < self._elecNoiseReducBufferSize:
+                if self._initialElecNoiseReduceBuffer < self._elecNoiseReducBufferSize:
                     readBytes = self._i2cBus.read_i2c_block_data(self._devAddress, 0x00, 2)
                     res = (readBytes[0] << 8) + readBytes[1]
                     self._elecNoiseReducBuffer.append(res)
-                    inStartUp += 1
+                    self._initialElecNoiseReduceBuffer += 1
                 else:
                     self._elecNoiseReducBuffer.pop(0)
                     readBytes = self._i2cBus.read_i2c_block_data(self._devAddress, 0x00, 2)
@@ -73,9 +92,48 @@ class BatteryWorker(QThread):
                         self.save_maxElecNumber(elecNumber)
 
                     self.set_level(int(((elecNumber - self._minElecNumber) / (self._maxElecNumber - self._minElecNumber)) * 100))
+                    self.new_levelRead(self._level)
                     time.sleep(0.1)
         except:
             pass
+
+
+    def new_levelRead(self, level: int):
+        if self._initialNoiseReduceBuffer < self._noiseReductionBufferSize:
+            self._noiseReductionBuffer.append(level)
+            self._initialNoiseReduceBuffer += 1
+
+        elif self._initialNoiseReduceBuffer == self._noiseReductionBufferSize:
+            self._noiseReductionBuffer.pop(0)
+            self._noiseReductionBuffer.append(level)
+            self.set_batteryLevel(int(self.outlierRemover(self._noiseReductionBuffer)))
+            self.newLevelSignal.emit(self._batteryLevel)
+            self._initialNoiseReduceBuffer += 1
+
+        else:
+            self._noiseReductionBuffer.pop(0)
+            self._noiseReductionBuffer.append(level)
+
+            newLevel = self.outlierRemover(self._noiseReductionBuffer, outlier_margin=1)
+            if newLevel < self._batteryLevel:
+                canChange = True
+                for level in self._noiseReductionBuffer:
+                    if self._batteryLevel < level:
+                        canChange = False
+                if canChange:
+                    self.stopChargingSignal.emit()
+                    self.set_batteryLevel(self._batteryLevel - 1)
+                    self.newLevelSignal.emit(self._batteryLevel)
+
+            elif newLevel > self._batteryLevel:
+                canChange = True
+                for level in self._noiseReductionBuffer:
+                    if level < self._batteryLevel:
+                        canChange = False
+                if canChange:
+                    self.chargingSignal.emit()
+                    self.set_batteryLevel(self._batteryLevel + 1)
+
 
     def save_minElecNumber(self, v: int):
         self._minElecNumber = v
