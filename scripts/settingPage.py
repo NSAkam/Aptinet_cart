7ae7c392043f2148024d8715158efecf68b21856
Mizmamad <moradlimohammad@gmail.com>
2023-12-12 05:31:09 +0330

    remove product test

--- scripts/settingPage.py
+++ scripts/settingPage.py
@@ -59,9 +59,11 @@ class SettingPage(QObject):
         if days >= self._calibrationPeriod:
             self.set_lastCalibrationDate(str(lastCalibrationDate.date()))
             self.expiredCalibrationSignal.emit(True)
+            print("expired")
         else:
             self.set_lastCalibrationDate(str(lastCalibrationDate.date()))
             self.expiredCalibrationSignal.emit(False)
+            print("not expired")
 
     ### Signals ########################################################################################################
     changedSignal = Signal()
